50a7588cacaea38b02a7d813c3ac7b8c15d537e8
Jeff Martin <jeffmartin@gmail.com>
2021-07-05 13:40:29 -0700

    move config.GetDefault to its own file

--- config/config.go
+++ config/config.go
@@ -56,39 +56,6 @@ func NewWithTerminalIO(overrides *Config, termio *TerminalIO) Config {
 	return cfg
 }
 
-func GetDefault() Config {
-	return Config{
-		Policies: []string{"conventional-lax", "lax"},
-		Branches: []string{"main", "master"},
-		CustomPolicies: []Policy{
-			{
-				Name:                  "conventional-lax",
-				SubjectRE:             `^(?P<type>[A-Za-z0-9]+)(?P<scope>\([^\)]+\))?!?:\s+(?P<body>.+)$`,
-				BodyAnnotationStartRE: `^(?P<type>[A-Z ]+): `,
-				BreakingChangeTypes:   []string{"BREAKING CHANGE"},
-				CommitTypes: map[string]string{
-					"feat":        "MINOR",
-					"fix":         "PATCH",
-					"revert":      "PATCH",
-					"cont":        "PATCH",
-					"perf":        "PATCH",
-					"improvement": "PATCH",
-					"refactor":    "PATCH",
-					"style":       "PATCH",
-					"test":        "SKIP",
-					"chore":       "SKIP",
-					"docs":        "SKIP",
-				},
-			},
-			{
-				Name:                "lax",
-				SubjectRE:           `^(?P<scope>[A-Za-z0-9_-]+): `,
-				FallbackReleaseType: "PATCH",
-			},
-		},
-	}
-}
-
 func (c Config) Validate() error {
 	if (c.Major && (c.Minor || c.Patch)) ||
 		(c.Minor && (c.Patch)) {
---
+++ config/default.go
@@ -0,0 +1,34 @@
+package config
+
+func GetDefault() Config {
+	return Config{
+		Policies: []string{"conventional-lax", "lax"},
+		Branches: []string{"main", "master"},
+		CustomPolicies: []Policy{
+			{
+				Name:                  "conventional-lax",
+				SubjectRE:             `^(?P<type>[A-Za-z0-9]+)(?P<scope>\([^\)]+\))?!?:\s+(?P<body>.+)$`,
+				BodyAnnotationStartRE: `^(?P<type>[A-Z ]+): `,
+				BreakingChangeTypes:   []string{"BREAKING CHANGE"},
+				CommitTypes: map[string]string{
+					"feat":        "MINOR",
+					"fix":         "PATCH",
+					"revert":      "PATCH",
+					"cont":        "PATCH",
+					"perf":        "PATCH",
+					"improvement": "PATCH",
+					"refactor":    "PATCH",
+					"style":       "PATCH",
+					"test":        "SKIP",
+					"chore":       "SKIP",
+					"docs":        "SKIP",
+				},
+			},
+			{
+				Name:                "lax",
+				SubjectRE:           `^(?P<scope>[A-Za-z0-9_-]+): `,
+				FallbackReleaseType: "PATCH",
+			},
+		},
+	}
+}
