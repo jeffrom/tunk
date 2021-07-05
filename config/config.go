a303a0b7a99799e2085234c6b63c09b5b46ba02b
Jeff Martin <jeffmartin@gmail.com>
2021-07-05 13:35:42 -0700

    chore: move config.Policy to its own file

--- config/config.go
+++ config/config.go
@@ -145,34 +145,3 @@ func oneOf(s string, l []string) bool {
 	}
 	return false
 }
-
-type Policy struct {
-	Name                  string            `json:"name"`
-	SubjectRE             string            `json:"subject_regex"`
-	BodyAnnotationStartRE string            `json:"body_annotation_start_regex"`
-	BreakingChangeTypes   []string          `json:"breaking_change_types"`
-	CommitTypes           map[string]string `json:"commit_types"`
-	FallbackReleaseType   string            `json:"fallback_type,omitempty"`
-	subjectRE             *regexp.Regexp
-	bodyRE                *regexp.Regexp
-}
-
-func (p *Policy) GetSubjectRE() *regexp.Regexp {
-	if p.SubjectRE == "" {
-		return nil
-	}
-	if p.subjectRE == nil {
-		p.subjectRE = regexp.MustCompile(p.SubjectRE)
-	}
-	return p.subjectRE
-}
-
-func (p *Policy) GetBodyAnnotationRE() *regexp.Regexp {
-	if p.BodyAnnotationStartRE == "" {
-		return nil
-	}
-	if p.bodyRE == nil {
-		p.bodyRE = regexp.MustCompile(p.BodyAnnotationStartRE)
-	}
-	return p.bodyRE
-}
---
+++ config/policy.go
@@ -0,0 +1,34 @@
+package config
+
+import "regexp"
+
+type Policy struct {
+	Name                  string            `json:"name"`
+	SubjectRE             string            `json:"subject_regex"`
+	BodyAnnotationStartRE string            `json:"body_annotation_start_regex"`
+	BreakingChangeTypes   []string          `json:"breaking_change_types"`
+	CommitTypes           map[string]string `json:"commit_types"`
+	FallbackReleaseType   string            `json:"fallback_type,omitempty"`
+	subjectRE             *regexp.Regexp
+	bodyRE                *regexp.Regexp
+}
+
+func (p *Policy) GetSubjectRE() *regexp.Regexp {
+	if p.SubjectRE == "" {
+		return nil
+	}
+	if p.subjectRE == nil {
+		p.subjectRE = regexp.MustCompile(p.SubjectRE)
+	}
+	return p.subjectRE
+}
+
+func (p *Policy) GetBodyAnnotationRE() *regexp.Regexp {
+	if p.BodyAnnotationStartRE == "" {
+		return nil
+	}
+	if p.bodyRE == nil {
+		p.bodyRE = regexp.MustCompile(p.BodyAnnotationStartRE)
+	}
+	return p.bodyRE
+}
